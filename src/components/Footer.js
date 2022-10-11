7d36a5e3d4b044c5f1c0b87701cc3dfd97523642
sifatullahsu <personal.sifat@gmail.com>
2022-10-11 21:43:35 +0600

    Footer Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer id='footer' className='text-center'>
+      <p>2022 All Right Reserved By <span className='text-indigo-800'><Link to='/'>DevQuiz</Link></span></p>
+    </footer>
   );
 };
 
